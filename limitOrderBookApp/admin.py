9e1a923bf637587baed185d20141fcb8c7ef214b
Coffe Cat <nikitatyshchenkoofficial@gmail.com>
2024-01-20 00:54:05 +0200

    Add Sstock, Order, Transactions models, register them into admin.py, add requirements.txt

--- limitOrderBookApp/admin.py
+++ limitOrderBookApp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from limitOrderBookApp.models import Order, Stock, Transactions
 
 # Register your models here.
+admin.site.register(Stock)
+admin.site.register(Order)
+admin.site.register(Transactions)
